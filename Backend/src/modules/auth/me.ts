f859854d597b1c22a34cf565c1cc4836b15e231c
ayushisnotalive <theayushchakraborty@gmail.com>
2026-08-31 15:55:19 +0530

    testing fixes

--- Backend/src/modules/auth/me.ts
+++ Backend/src/modules/auth/me.ts
@@ -1,27 +1,21 @@
-import type{ AuthRequest } from "../../api/middleware/authenticate";
+import type { AuthRequest } from "../../api/middleware/authenticate";
 import type { Response } from "express";
 import { db } from "../../infrastructure/DB/db";
 
-
-
-
-export const Me = async(req:AuthRequest, res:Response)=>{
-    try{
-        console.log("DEBUG - req.userId:", req.userId);
-
+export const Me = async (req: AuthRequest, res: Response) => {
+    try {
         const result = await db.query(
-            `SELECT id, name, email FROM booking.users where id = $1`,
+            `SELECT id, name, email FROM booking.users WHERE id = $1`,
             [req.userId]
         );
 
-        console.log("DEBUG - rows found:", result.rows.length);
-        if(result.rows.length===0){
-             return res.status(401).json({ success: false, message: "User not found." });
-    }
+        if (result.rows.length === 0) {
+            return res.status(401).json({ success: false, message: "User not found." });
+        }
 
-    return res.status(200).json({ success: true, user: result.rows[0] });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ success: false, message: "Internal Server Error" });
-  }
+        return res.status(200).json({ success: true, user: result.rows[0] });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
 };
