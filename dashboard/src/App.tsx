2f2a757d908442299d1108bcc6fe3afa70987785
ayushisnotalive <theayushchakraborty@gmail.com>
2026-08-31 14:18:36 +0530

    protected routes fixes testing

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
+import { ProtectedRoute } from './components/protectedRoutes';
 // import { ProtectedRoute } from './components/protectedRoutes';
 
 function App() {
@@ -12,8 +13,9 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={
-          
+          <ProtectedRoute>
             <Dashboard/>
+          </ProtectedRoute>
         
           } />
       </Routes>
