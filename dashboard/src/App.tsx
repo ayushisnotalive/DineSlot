1d45f8060646800ecdded7e25ef3e0fb92f9fa26
ayushisnotalive <theayushchakraborty@gmail.com>
2026-08-31 14:04:59 +0530

    checking for error

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
-import { ProtectedRoute } from './components/protectedRoutes';
+// import { ProtectedRoute } from './components/protectedRoutes';
 
 function App() {
   return (
