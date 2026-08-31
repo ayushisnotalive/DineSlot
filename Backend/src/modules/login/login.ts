2512254cf50e75a981f0bcc3ca5bb5bd97faf38f
ayushisnotalive <theayushchakraborty@gmail.com>
2026-08-31 21:55:40 +0530

    implemented logout-refreshtoken-rotation

--- Backend/src/modules/login/login.ts
+++ Backend/src/modules/login/login.ts
@@ -46,8 +46,9 @@ export const login = async (req: Request, res: Response) => {
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
             secure: true,
-            sameSite: "none",
+            sameSite: "none" as const,
             maxAge: 7 * 24 * 60 * 60 * 1000,
+            path: "/api/auth/refresh",
         });
 
         return res.status(200).json({
